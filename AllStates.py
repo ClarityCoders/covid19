289808d6b862319e79dd6df051f2c6b6da0617eb
Jake Lower <jake@claritycoders.com>
2020-04-11 11:13:22 -0500

    Comments and such...

--- AllStates.py
+++ AllStates.py
@@ -1,22 +1,28 @@
 from matplotlib import pyplot as plt
 import pandas as pd
 
+
 df = pd.read_csv('us-counties.csv')
 
+# Used this originally to iterate over all states.
+# Currently not using and only going over top 10 states.
 unique_states = df['state'].unique()
 
+# Get last date to see which states have the most cases currently
 last_date = df['date'].max()
 df_last_date = df[ df['date'] == last_date]
 series_last_date = df_last_date.groupby('state')['cases'].sum()
-series_last_date = series_last_date.nlargest(5)
+series_last_date = series_last_date.nlargest(10)
 
+# The chat is flat before 3/15 so lets limit it to after that date.
 date_after = pd.Timestamp("03/15/2020")
 
+# Iterate over only the states in top 10
 for state in series_last_date.index:
     df_state = df[ df['state'] == state].copy()
     df_state['date'] = pd.to_datetime(df_state['date'])
+    # Added this to limit dates plotted to after 3/15
     df_state = df_state[ df_state['date'] > date_after]
-    
     series_state = df_state.groupby('date')['cases'].sum()
     series_state = series_state.diff()
     plt.plot(series_state.index, series_state.values, label=state)
--- PlotState.py
+++ PlotState.py
@@ -1,21 +1,20 @@
+# Example showing how to plot a single state using a downloaded csv
 from matplotlib import pyplot as plt
 import pandas as pd
 
+# Pull in our csv file make sure its in same directory.
 df = pd.read_csv("us-counties.csv")
-print(df)
 
-# Style Options do this last
-# plt.style.use('seaborn-deep')
-
-# do wihtout copy explain error
-#df_illinois = df[ df['state'] == 'Illinois']
+# Create a new data frame of a single state.
 df_illinois = df[ df['state'] == 'Illinois'].copy()
+# Make sure pandas realizes our date column is a date.
 df_illinois['date'] = pd.to_datetime(df_illinois['date'])
 
+# Sum up all the cases for Illinois by date.
+# Use diff method to compair to previous day to get new cases.
 series_illinois_sum = df_illinois.groupby('date')['cases'].sum()
 series_illinois_diff = series_illinois_sum.diff()
 
-#series_illinois = series_illinois.diff()
 plt.plot(series_illinois_diff.index, series_illinois_diff.values, label="Illinois")
 #plt.xlabel("Date")
 plt.ylabel("New Cases")
